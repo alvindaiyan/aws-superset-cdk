9d866ba4a5b8f3b68f9e6348caca585f8cf9712d
Yan Dai <cyanda@amazon.com>
2023-01-28 16:38:42 +0800

    fixing pipeline

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,7 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'superset-cdk',
-  gitignore: ['.idea/'],
+  gitignore: ['.idea/', '__snapshots__/', 'cdk.context.json'],
   mutableBuild: true,
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
